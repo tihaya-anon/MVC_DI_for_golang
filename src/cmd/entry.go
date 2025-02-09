1056d12846b0bf574d895f86a22b3ed6a9e119ee
tihaya-anon <smsmun.luoyx@outlook.com>
2025-02-09 23:49:02 +0800

    refactor: :recycle: remove unused mods

--- src/cmd/entry.go
+++ src/cmd/entry.go
@@ -2,14 +2,11 @@ package cmd
 
 import (
 	"MVC_DI/config"
-	user_controller_builder "MVC_DI/section/user/controller/builder"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Start() {
 	router := gin.Default()
-	user_controller_builder.NewUserEntryControllerBuilder().Build().RegisterRoutes(router)
-	user_controller_builder.NewUserAuthControllerBuilder().Build().RegisterRoutes(router)
 	router.Run(config.Application.App.Uri)
 }
