7593c1be42dcb926f11ee58b6ab511a154dbd8bd
tihaya-anon <smsmun.luoyx@outlook.com>
2025-02-14 13:02:30 +0800

    refactor(router): :recycle: split initRouter func into 3 func
    split the logic into create->run->stop

--- src/cmd/entry.go
+++ src/cmd/entry.go
@@ -2,19 +2,41 @@ package cmd
 
 import (
 	"MVC_DI/config"
-	"MVC_DI/router"
 	user_router "MVC_DI/router/user"
 	user_controller_builder "MVC_DI/section/user/controller/builder"
+	"MVC_DI/server"
+	"context"
 	"fmt"
+	"os/signal"
+	"syscall"
+	"time"
 )
+
+func bindController() {
+	userEntryController := user_controller_builder.NewUserEntryControllerBuilder().Build()
+	user_router.BindUserEntryController(userEntryController)
+
+	userAuthController := user_controller_builder.NewUserAuthControllerBuilder().Build()
+	user_router.BindUserAuthController(userAuthController)
+}
+
+func startServer(publicPath, authPath string, timeOut time.Duration) {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	server := server.NewServer()
+	server.Setup(publicPath, authPath)
+	server.Run()
+
+	<-ctx.Done()
+
+	server.Stop(timeOut)
+}
 
 func Start() {
 	config.InitConfig()
-	userEntryController := user_controller_builder.NewUserEntryControllerBuilder().Build()
-	userAuthController := user_controller_builder.NewUserAuthControllerBuilder().Build()
-	user_router.BindUserEntryController(userEntryController)
-	user_router.BindUserAuthController(userAuthController)
-	router.InitRouter()
+	bindController()
+	startServer("/api/v1/public", "/api/v1", 5*time.Second)
 }
 
 func Stop() {
--- src/router/router.go
+++ src/router/router.go
@@ -1,65 +1,16 @@
 package router
 
 import (
-	"MVC_DI/config"
-	"context"
-	"fmt"
-	"net/http"
-	"os/signal"
-	"syscall"
-	"time"
-
 	"github.com/gin-gonic/gin"
 )
 
 type IRegisterRouterFunc = func(publicRouterGroup *gin.RouterGroup, authRouterGroup *gin.RouterGroup)
 
-var registerRouterFuncList []IRegisterRouterFunc
+var RegisterRouterFuncList []IRegisterRouterFunc
 
 func RegisterRouter(fn IRegisterRouterFunc) {
 	if fn == nil {
 		return
 	}
-	registerRouterFuncList = append(registerRouterFuncList, fn)
-}
-
-// # InitRouter
-//
-// InitRouter initializes the router and starts the server.
-// It will iterate over the registerRouterFuncList and call each function
-// to register the routes. Then it will start the server and wait for
-// the context to be done.
-func InitRouter() {
-	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer cancel()
-
-	// create the gin router
-	router := gin.Default()
-
-	publicRouterGroup := router.Group("/api/v1/public")
-	authRouterGroup := router.Group("/api/v1")
-
-	for _, registerRouterFunc := range registerRouterFuncList {
-		registerRouterFunc(publicRouterGroup, authRouterGroup)
-	}
-
-	server := &http.Server{Addr: config.Application.App.Uri, Handler: router}
-
-	go func() {
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			fmt.Printf("Start server failed: %s\n", err)
-			return
-		}
-	}()
-
-	<-ctx.Done()
-
-	ctx, cancelShutdowm := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancelShutdowm()
-
-	if err := server.Shutdown(ctx); err != nil {
-		fmt.Printf("Server shutdown failed: %s\n", err)
-		return
-	}
-	fmt.Println("Server shutdown successfully")
+	RegisterRouterFuncList = append(RegisterRouterFuncList, fn)
 }
